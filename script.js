bcf34f5d8df0f86b19d0be068adf9c805706b0c1
az <azabrodi@genesys.com>
2017-10-09 01:37:15 +0300

    +lamp

--- script.js
+++ script.js
@@ -1,4 +1,107 @@
 var playlist=[];
+var lamp_port=8080;
+var lamp_url=null;
+
+function getHighOfMyIp() {
+    var a=$("<a></a>");
+    a.prop("href",document.location);
+    var host=a.prop("hostname");
+    var splittedHost=host.split(".",4);
+    if (splittedHost.length!=4) { alert("Can't get own IP:"+splittedHost); return null; }
+    var myIpHigh=splittedHost[0]+'.'+splittedHost[1]+'.'+splittedHost[2]+'.';
+    return [ myIpHigh, Number(splittedHost[3]) ];
+}
+
+var lampRequestInProgress=null;
+
+function doLampRequest(cmd,onOk,onFail,url) {
+    if (lampRequestInProgress!=null) {
+        if (lampRequestInProgress.nextRequest!=null) {
+            onFail();
+            return;
+        }
+        lampRequestInProgress.nextRequest={
+            "cmd":cmd, "onOk":onOk, "onFail":onFail, "url":url
+        }
+        return;
+    }
+
+    var settings=
+        {
+            "cache": false,
+            "success": function(data) {
+                var next=lampRequestInProgress.nextRequest; lampRequestInProgress=null;
+                onOk(data);
+                doLampRequest(next.cmd, next.onOk, next.onFail, next.url);
+            },
+            "error": function() {
+                var next=lampRequestInProgress.nextRequest; lampRequestInProgress=null;
+                onFail();
+                doLampRequest(next.cmd, next.onOk, next.onFail, next.url);
+            },
+            "dataType": "text",
+            "timeout": 1000,
+            "nextRequest": null
+        };
+    
+    lampRequestInProgress=settings;
+    $.ajax((url==null ? lamp_url : url)+"/"+cmd+".", settings);
+}
+
+var lampErrCnt=0;
+
+function lampTest() {
+    doLampRequest(
+        "test",
+        function() {
+            $("#lamp-status").hide(500);
+            $("#lamp-controls").show(500);
+            lampErrCnt=0; setTimeout(lampTest,20000);
+        },
+        function() {
+            $("#lamp-status").show(500);
+            $("#lamp-controls").hide(500);
+            if (++lampErrCnt>10) findDrunkenLamp();
+            setTimeout(lampTest,5000);
+        }
+    );
+}
+
+function lampControl(cmd) {
+    doLampRequest(cmd);
+}
+
+function findDrunkenLamp_iter(iph,ipl) {
+    console.info("LAMP_SEARCH try:"+iph+ipl);
+    doLampRequest(
+        "test",
+        function(data) {
+                if (data=="LAMP") {
+                    lamp_url="http://"+iph+ipl+":"+lamp_port;
+                    console.info("LAMP_FOUND URL="+lamp_url)
+                    $("#lamp-status").hide(500);
+                    $("#lamp-controls").show(500);
+                    lampErrCnt=0;
+                    setTimeout(lampTest,20000);
+                } else {
+                    setting.error();
+                }
+        },
+        function() {
+            findDrunkenLamp_iter(iph, ipl<254 ? ipl+1 : 2);
+        },
+        "http://"+iph+ipl+":"+lamp_port
+     );
+}
+
+
+function findDrunkenLamp() {
+    $("#lamp-status").show();
+    $("#lamp-controls").hide();
+    var x=getHighOfMyIp();
+//    findDrunkenLamp_iter(x[0], x[1]>=7 ? x[1]-5 : 2);
+    if (x!=null) findDrunkenLamp_iter(x[0], 170);
+}
 
 function showImage(index) {
     $.get("",{"image":index})
@@ -91,9 +194,20 @@ $(function() {
     $("#pausevideo").click( pauseVideo );
     $("#continuevideo").click( continueVideo );
     
-    $( "#mainvolume" ).slider({ change: function(event, ui) { setMainVolume(ui.value) } });
-    $( "#audiovolume" ).slider({ change: function(event, ui) { setAudioVolume(ui.value) } });
-    $( "#videovolume" ).slider({ change: function(event, ui) { setVideoVolume(ui.value) } });
+//    $("#mainvolume").change(function() { setMainVolume(this.value) } );
+//    $("#audiovolume").change(function() { setAudioVolume(this.value) } );
+//    $("#videovolume").change(function() { setVideoVolume(this.value) } );
+    $("#mainvolume").get(0).oninput=function() { setMainVolume(this.value) };
+    $("#audiovolume").get(0).oninput=function() { setAudioVolume(this.value) };
+    $("#videovolume").get(0).oninput=function() { setVideoVolume(this.value) };
+
+    findDrunkenLamp();
+    $("#lamp-on").click(function() { lampControl("on"); });
+    $("#lamp-off").click(function() { lampControl("off"); });
+    $("#lamp-blink").click(function() { lampControl("blink"); });
+    $("#lamp-sound1").click(function() { lampControl("sound1"); });
+    $("#lamp-sound2").click(function() { lampControl("sound2"); });
+    $("#lamp-burn").click(function() { lampControl("burn"); });
 
 })
 
